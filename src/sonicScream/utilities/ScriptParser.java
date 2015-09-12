621c75435a747891d2544be038e329f0357cf14c
pingzing <neil.mcalister@futurice.com>
2015-09-12 13:30:52 +0300

    Parser now works on all tested files

--- src/sonicScream/utilities/ScriptParser.java
+++ src/sonicScream/utilities/ScriptParser.java
@@ -34,62 +34,75 @@ import javafx.scene.control.TreeItem;
 
 /**
  *
- * @author
- * Neil
- * McAlister
+ * @author Neil McAlister
  */
 public class ScriptParser
 {
-    private static List<TreeItem<String>> _scriptTree = null;    
+
+    private static List<TreeItem<String>> _scriptTree = null;
     private static TreeItem<String> _currentNode = null;
-    private static TreeItem<String> _currentParent = null;            
-    
+    private static TreeItem<String> _currentParent = null;
+
     public static List<TreeItem<String>> parseScript(File scriptFile) throws IOException
     {
         _scriptTree = new ArrayList<TreeItem<String>>();
         _currentNode = new TreeItem<>();
         _scriptTree.add(_currentNode);
-        _currentParent = _currentNode;               
-        
-        try(
-            FileInputStream fis = new FileInputStream(scriptFile);
-            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-            )
-        {                                  
+        _currentParent = _currentNode;
+
+        try (
+                FileInputStream fis = new FileInputStream(scriptFile);
+                BufferedReader br = new BufferedReader(new InputStreamReader(fis));)
+        {
             parseHeaderAndFirstTwoLines(br, StringParsing.GetScriptNameFromFileName(scriptFile.getName()));
-                        
+
+            Boolean bracketAdded = false; 
             String line;
-            while((line = br.readLine()) != null)
-            {                                
-                parseLine(line);            
+            while ((line = br.readLine()) != null)
+            {
+                if (line.contains("operator_stacks"))
+                {
+                    parseNameAndOperatorStacks(line);
+                    bracketAdded = false;
+                }
+                else
+                {
+                    parseLine(line);
+                }
+                
+                if(line.contains("}") && !bracketAdded)
+                {
+                    parseLine("}");
+                    bracketAdded = true;
+                }
             }
-            return _scriptTree;                     
+            return _scriptTree;
         }
-    }    
-    
+    }
+
     //This strips out all the header junk and correctly separates and parses the first two lines of the actual script.
     private static void parseHeaderAndFirstTwoLines(BufferedReader br, String scriptSubject) throws IOException
     {
         int seenCount = 0;
         //Seek to the end of the header                                                  
-        while(seenCount < 2)
+        while (seenCount < 2)
         {
             String line = br.readLine();
             int dataStartIndex = line.indexOf(scriptSubject);
-            if(dataStartIndex != -1)
+            if (dataStartIndex != -1)
             {
                 seenCount++;
             }
-            
+
             //Sometimes the entire header is a single line. Check for a second occurrence immediately.
-            if(seenCount < 2 && line.lastIndexOf(scriptSubject) != dataStartIndex)
+            if (seenCount < 2 && line.lastIndexOf(scriptSubject) != dataStartIndex)
             {
                 dataStartIndex = line.lastIndexOf(scriptSubject);
                 seenCount++;
             }
-                
-            if(seenCount == 2)
-            {                    
+
+            if (seenCount == 2)
+            {
                 line = line.substring(dataStartIndex, line.length() - 1);
                 int separatorIndex = line.indexOf("\"");
                 //String firstLine = "\"" + line.substring(0, separatorIndex-1) + "\"";
@@ -97,14 +110,24 @@ public class ScriptParser
                 //now we have the sound name and "operator stacks" mashed together 
                 //like so: heroname"operator stacks" we need to add a quote to 
                 //the beginning, then separate these two.                
-                parseLine(line);                                                        
+                parseNameAndOperatorStacks(line);
             }
-        }  
+        }
+    }
+
+    private static void parseNameAndOperatorStacks(String line)
+    {
+        int separatorIndex = line.indexOf("\"");
+        String name = "\"" + line.substring(0, separatorIndex);
+        String operator = "\"" + line.substring(separatorIndex, line.length());
+        parseLine(name);
+        parseLine("{");
+        parseLine(operator);
     }
 
     private static void parseLine(String line) throws NullPointerException
-    {                
-        if (line.isEmpty() || line.equals("") || line.contentEquals("\t") 
+    {
+        if (line.isEmpty() || line.equals("") || line.contentEquals("\t")
                 || line.trim().length() <= 0)
         {
             return;
@@ -125,22 +148,22 @@ public class ScriptParser
             {
                 ex.printStackTrace();
             }
-        }        
+        }
         else if (line.contains("{"))
-        {            
-            _currentParent = _currentNode;            
+        {
+            _currentParent = _currentNode;
         }
         else if (line.contains("}"))
-        {            
+        {
             _currentParent = _currentParent.getParent();
             if (_currentParent == null)
             {
                 _currentParent = _scriptTree.get(0);
-                if(_currentParent.getParent() != null)
+                if (_currentParent.getParent() != null)
                 {
                     throw new AssertionError("_scriptTree[0] is NOT the root element!");
                 }
-            }            
+            }
         }
         else
         {
@@ -166,7 +189,6 @@ public class ScriptParser
 //    {
 //        //if(line.startsWith(line))
 //    }
-
     private StringBuilder parseModel(List<TreeItem<String>> currentTreeModel)
     {
         TreeItem<String> root = currentTreeModel.get(0);
